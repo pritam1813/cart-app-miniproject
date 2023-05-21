2952dbf9b1ba27578e48ec02d3daa451fff06ac7
Pritam Dhara <pritam1813@gmail.com>
2023-05-21 14:28:25 +0530

    Added increase quantity function

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Add from '../assets/add.png';
+import Add from "../assets/add.png";
 import minus from "../assets/minus.png";
 import remove from "../assets/delete.png";
 import phone from "../assets/phone.avif";
@@ -20,7 +20,12 @@ class CartItem extends React.Component<{}, ComponentState> {
       title: "mobile Phone",
       img: "",
     };
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
+
+  increaseQuantity = () => {
+    console.log("this: ", this.state.Qty);
+  };
 
   render() {
     const { price, Qty, title } = this.state;
@@ -36,7 +41,12 @@ class CartItem extends React.Component<{}, ComponentState> {
 
           <div className="cart-item-actions">
             {/* Buttons */}
-            <img className="action-icons" src={Add} alt="increase quantity" />
+            <img
+              className="action-icons"
+              src={Add}
+              alt="increase quantity"
+              onClick={this.increaseQuantity}
+            />
             <img className="action-icons" src={minus} alt="decrease quantity" />
             <img className="action-icons" src={remove} alt="remove item" />
           </div>
