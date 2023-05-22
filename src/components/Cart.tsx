12db219c9e55c74f4bf3da6a5e1a10221170b2b0
Pritam Dhara <pritam1813@gmail.com>
2023-05-22 22:32:53 +0530

    Used Props to chnge states from cart to cartitems

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,13 +1,104 @@
 import React from "react";
 import CartItem from "./CartItem";
 
-class Cart extends React.Component {
+type Product = {
+  price: number;
+  title: string;
+  qty: number;
+  img: string;
+  id: number;
+};
+
+type CartState = {
+  products: Product[];
+};
+
+class Cart extends React.Component<{}, CartState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      products: [
+        {
+          price: 99,
+          title: "Watch",
+          qty: 1,
+          img: "",
+          id: 1,
+        },
+        {
+          price: 999,
+          title: "Mobile Phone",
+          qty: 10,
+          img: "",
+          id: 2,
+        },
+        {
+          price: 999,
+          title: "Laptop",
+          qty: 4,
+          img: "",
+          id: 3,
+        },
+      ],
+    };
+  }
+
+  handleIncreaseQuantity = (product: Product): void => {
+    this.setState((prevState) => {
+      const updatedProducts = prevState.products.map((p) => {
+        if (p.id === product.id) {
+          return {
+            ...p,
+            qty: p.qty + 1,
+          };
+        }
+        return p;
+      });
+      return {
+        products: updatedProducts,
+      };
+    });
+  };
+
+  handleDecreaseQuantity = (product: Product): void => {
+    this.setState((prevState) => {
+      const updatedProducts = prevState.products.map((p) => {
+        if (p.id === product.id && p.qty > 0) {
+          return {
+            ...p,
+            qty: p.qty - 1,
+          };
+        }
+        return p;
+      });
+      return {
+        products: updatedProducts,
+      };
+    });
+  };
+
+  handleDeleteProduct = (id: number): void => {
+    this.setState((prevState) => {
+      const updatedProducts = prevState.products.filter((p) => p.id !== id);
+      return {
+        products: updatedProducts,
+      };
+    });
+  };
+
   render() {
+    const { products } = this.state;
     return (
       <div className="cart-wrap">
-        <CartItem />
-        <CartItem />
-        <CartItem />
+        {products.map((product) => (
+          <CartItem
+            key={product.id}
+            product={product}
+            onIncreaseQuantity={this.handleIncreaseQuantity}
+            onDecreaseQuantity={this.handleDecreaseQuantity}
+            onDeleteProduct={this.handleDeleteProduct}
+          />
+        ))}
       </div>
     );
   }
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -4,67 +4,34 @@ import minus from "../assets/minus.png";
 import remove from "../assets/delete.png";
 import phone from "../assets/phone.avif";
 
-type ComponentState = {
+type Product = {
   price: number;
-  Qty: number;
   title: string;
+  qty: number;
   img: string;
+  id: number;
 };
 
-class CartItem extends React.Component<{}, ComponentState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      price: 9999,
-      Qty: 1,
-      title: "mobile Phone",
-      img: "",
-    };
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-  }
+type ComponentProps = {
+  product: Product;
+  onIncreaseQuantity: (product: Product) => void;
+  onDecreaseQuantity: (product: Product) => void;
+  onDeleteProduct: (id: number) => void;
+};
 
-  increaseQuantity = () => {
-    console.log("this: ", this.state.Qty);
-
-    //If we dont require previous state then use this
-    // this.setState({
-    //   Qty: this.state.Qty + 1
-    // });
-
-    //Else Use this
-    //Set state is a function from React.Component
-    this.setState((prevState) => {
-      return {
-        Qty: prevState.Qty + 1,
-      };
-    });
-  };
-
-  decreaseQuantity = () => {
-    this.setState((prevState) => {
-      if (prevState.Qty > 1) {
-        return {
-          Qty: prevState.Qty - 1,
-        };
-      } else {
-        return {
-          Qty: prevState.Qty,
-        };
-      }
-    });
-  };
-
+class CartItem extends React.Component<ComponentProps> {
   render() {
-    const { price, Qty, title } = this.state;
+    const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } =
+      this.props;
     return (
       <div className="cart-item">
         <div className="left-block">
           <img style={styles.image} src={phone} alt="Phone Image" />
         </div>
         <div className="right-block">
-          <div style={{ fontSize: 25 }}>{title}</div>
-          <div>Rs. {price}</div>
-          <div>Quantity: {Qty}</div>
+          <div style={{ fontSize: 25 }}>{product.title}</div>
+          <div>Rs. {product.price}</div>
+          <div>Quantity: {product.qty}</div>
 
           <div className="cart-item-actions">
             {/* Buttons */}
@@ -72,15 +39,20 @@ class CartItem extends React.Component<{}, ComponentState> {
               className="action-icons"
               src={Add}
               alt="increase quantity"
-              onClick={this.increaseQuantity}
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               className="action-icons"
               src={minus}
               alt="decrease quantity"
-              onClick={this.decreaseQuantity}
+              onClick={() => onDecreaseQuantity(product)}
             />
-            <img className="action-icons" src={remove} alt="remove item" />
+            <img
+              className="action-icons"
+              src={remove}
+              alt="remove item"
+              onClick={() => onDeleteProduct(product.id)}
+            />
           </div>
         </div>
       </div>
